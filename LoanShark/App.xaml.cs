042be36e97fb2d34da710da13a62b73784d3b8a0
xela <perocho.xela@gmail.com>
2023-04-24 11:42:02 -0400

    init commit

---
+++ LoanShark/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoanShark;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ LoanShark/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LoanShark;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
